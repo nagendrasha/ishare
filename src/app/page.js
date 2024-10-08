38c9312399bd015e039c695d5357f11029c8d257
nagendra <nagendrasha191@gmail.com>
2024-10-08 11:38:19 +0530

    First Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,89 @@
-import Image from "next/image";
+'use client'
+import { useRef } from "react";
+import { toBlob } from "html-to-image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const imageRef = useRef(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleShare = async () => {
+    const newFile = await toBlob(imageRef.current);
+    const data = {
+      files: [
+        new File([newFile], "nuzlocke.png", {
+          type: newFile.type
+        })
+      ],
+      title: "Nuzlocke",
+      text: "Nuzlocke"
+    };
+
+    try {
+      if (!navigator.canShare(data)) {
+        console.error("Can't share");
+      }
+      await navigator.share(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <div className="App">
+      <h1>Dynamically generate and share images</h1>
+      <button onClick={handleShare} type="button">
+        Share
+      </button>
+      <span>
+        Credit for image: https://codepen.io/alvaromontoro/pen/BaKQzgG
+      </span>
+      <div className="cartoon__container" ref={imageRef}>
+        <div className="cartoon hb">
+          <div className="shadow"></div>
+          <div className="minar-top minar-top-2"></div>
+          <div className="minar-top"></div>
+          <div className="minar ha"></div>
+          <div className="tower tower-5 tower-6 tower-7"></div>
+
+          <div className="house ha"></div>
+
+          <div className="main-roof-behind"></div>
+          <div className="main-roof ha"></div>
+          <div className="main-top"></div>
+
+          <div className="flag flag-4"></div>
+          <div className="main-tower-roof"></div>
+          <div className="main-tower"></div>
+          <div className="main ha"></div>
+          <div className="tower tower-3 tower-3a"></div>
+          <div className="wall ha"></div>
+          <div className="tower tower-1"></div>
+          <div className="tower tower-2"></div>
+          <div className="tower tower-3 tower-3b"></div>
+          <div className="tower tower-4"></div>
+          <div className="tower tower-5 tower-6"></div>
+          <div className="tower tower-5"></div>
+          <div className="brick"></div>
+
+          <div className="room-1"></div>
+          <div className="room-2"></div>
+          <div className="room-3 ha"></div>
+          <div className="window"></div>
+          <div className="window-curved"></div>
+          <div className="roof roof-1"></div>
+          <div className="roof roof-2"></div>
+          <div className="flag flag-1"></div>
+          <div className="flag flag-2"></div>
+          <div className="flag flag-3"></div>
+          <div className="flag-pole-top"></div>
+          <div className="flag-pole"></div>
+          <div className="house-roof"></div>
+          <div className="front-gate ha">
+            <div className="gate"></div>
+            <div className="balcony ha"></div>
+            <div className="triangle ha"></div>
+            <div className="front-poles"></div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
