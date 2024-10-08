caed55bf550657dfb24d062f82ef2733b0b8315a
nagendra <nagendrasha191@gmail.com>
2024-10-08 12:00:07 +0530

    new code

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import SharePopup from './SharePopup';
+import Image from "next/image";
+import SharePopup from "./Sharepopup";
 
-function Home() {
+export default function Home() {
+
   const imageUrl = 'https://mir-s3-cdn-cf.behance.net/projects/404/5f3fa8209214981.Y3JvcCwxNDU2LDExMzgsMCww.jpg'; // Replace with your image URL
+
 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
-      <SharePopup imageUrl={imageUrl} />
-    </div>
+    <SharePopup imageUrl={imageUrl} />
+  </div>
   );
 }
-
-export default Home;
